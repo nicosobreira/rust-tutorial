94a442d3f69ab754778e3e41fa00c289a8ed2775
nicosobreira <nicolau.sobreira@gmail.com>
2026-04-03 20:25:51 -0300

    a

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind};
+
+pub struct Config {
+    pub pattern: String,
+    pub file_path: String,
+}
+struct Match {
+    string: String,
+    line: usize,
+}
+
+fn run(config: &Config) -> io::Result<()> {
+    let file = file_open(&config.file_path);
+
+    let reader = BufReader::new(file);
+
+    let mut _matches: u32 = 0;
+    for (n, line) in reader.lines().enumerate() {
+        let line = line?;
+
+        if line.contains(&config.pattern) {
+            println!("{n}: {line}");
+            _matches += 1;
+        }
+    }
+
+    if _matches == 0 {
+        eprintln!("No matches found!");
+    }
+
+    Ok(())
+}
+
 // FIX: The function wiil not panic if the "name" is a directory
 // FIX: It also need to failed if the user doesn't have the permission to acess the file
 fn file_open(name: &str) -> File {
@@ -7,4 +42,45 @@ fn file_open(name: &str) -> File {
         }
         _ => panic!("{error}"),
     })
+}
+
+fn find_pattern(reader: &mut BufReader<File>, pattern: &str) -> Option<Vec<Match>> {
+    let mut matches = Vec::new();
+
+    for (n, line) in reader.lines().enumerate() {
+        let line = line.expect("a");
+
+        if line.contains(pattern) {
+            let matc = Match::new(&line, n);
+            matches.push(matc);
+        }
+    }
+
+    if matches.len() == 0 {
+        return None;
+    } else {
+        return Some(matches);
+    }
+}
+
+impl Config {
+    fn parse(args: &[String]) -> Self {
+        if args.len() != 3 {
+            panic!("This program only supports two arguments!");
+        }
+
+        let pattern = args[1].clone();
+        let file_path = args[2].clone();
+
+        Self { pattern, file_path }
+    }
+}
+
+impl Match {
+    fn new(string: &str, line: usize) -> Self {
+        Self {
+            string: String::from(string),
+            line: line,
+        }
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,34 +1,9 @@
 use std::env;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader, ErrorKind};
+use std::io::Result;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
+    let config = Config::parse(&args);
 
-    if args.len() != 3 {
-        panic!("This program only supports two arguments!");
-    }
-
-    let pattern = args.get(1).expect("Plese, enter a match!");
-    let file_name = args.get(2).expect("Please, enter a file name!");
-
-    let file = file_open(file_name);
-
-    let reader = BufReader::new(file);
-
-    let mut _matches: u32 = 0;
-    for (n, line) in reader.lines().enumerate() {
-        let line = line?;
-
-        if line.contains(pattern) {
-            println!("{n}: {line}");
-            _matches += 1;
-        }
-    }
-
-    if _matches == 0 {
-        eprintln!("No matches found!");
-    }
-
-    Ok(())
+    run()?
 }
